b96a18300e3fb372ab356e06d1b26e3302940bfb
Sanidhya Pratap Singh <sanidhya235@gmail.com>
2023-06-24 18:46:47 +0530

    write data to MongoDB

--- src/gitpulse/ops/__init__.py
+++ src/gitpulse/ops/__init__.py
@@ -2,6 +2,8 @@ from dagster import op
 import os
 from github import Github
 from github import Auth
+from pymongo import MongoClient
+from pymongo import InsertOne
 
 
 @op
@@ -19,7 +21,8 @@ def get_repositories(context) -> list:
 @op(out={})
 def get_pull_requests(context, repos: list):
     """
-    This Op returns the pull requests in a given repository
+    This Op extracts open pull requests in a given repository and writes 
+    their metadata to MongoDB collection `open_pull_requests`
     """
     # using an access token
     auth = Auth.Token(os.environ['GITHUB_TOKEN'])
@@ -27,15 +30,47 @@ def get_pull_requests(context, repos: list):
     # Public Web Github
     g = Github(auth=auth)
 
+    # list to store InsertOne objects for bulk write
+    inserts = []
+
     # loop over the repositories
     for repo in repos:
         # Replace 'owner' and 'repo' with the repository details
-        repo = g.get_repo('Stability-AI/stablediffusion')
+        repo = g.get_repo(f'{repo["repo_owner"]}/{repo["repo_name"]}')
 
         # Get the list of pull requests
         pulls = repo.get_pulls()
 
-        # Iterate over the pull requests and print their titles and URLs
+        # Iterate over the pull requests and create insert payload
         for pull in pulls:
-            context.log.info(f"Title: {pull.title}")
-            context.log.info(f"URL: {pull.html_url}")
+            inserts.append(
+                InsertOne(
+                    {
+                        "additions": pull.additions,
+                        "deletions": pull.deletions,
+                        "state": pull.state,
+                        "html_url": pull.html_url,
+                        "assignee": pull.assignee.login if pull.assignee else None,
+                        "assignees": [assignee.login for assignee in pull.assignees],
+                        "changed_files": pull.changed_files,
+                        "created_at": pull.created_at,
+                        "updated_at": pull.updated_at,
+                        "mergeable": pull.mergeable,
+                        "number": pull.number,
+                        "requested_reviewers": [reviewer.login for reviewer in pull.requested_reviewers],
+                        "state": pull.state,
+                        "title": pull.title,
+                        "user": pull.user.login
+                    }
+                )
+            )
+
+    # connect to MongoDB collection
+    mongo_client = MongoClient(os.environ["MONGO_CONNECTION_STRING"])
+    mongo_database = mongo_client["GitPulse"]
+    mongo_collection = mongo_database["open_pull_requests"]
+
+    # truncate collection and write
+    mongo_collection.delete_many({})
+    mongo_collection.bulk_write(inserts)
+    
